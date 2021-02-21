825a48ee4a7e572adaccf5759fe4f576e326a495
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-02-21 20:38:17 +0200

    Created files

---
+++ assets/js/TaskService.js
@@ -0,0 +1,21 @@
+const taskManager =(function(){
+    
+     class Task{
+         constructor(name){
+             this.name=name;
+         }
+     }
+     class TaskManager{
+         constructor(){
+             this.tasks=[];
+         }
+          addTask(another) {
+              
+          }
+          removeTask(another) {
+              
+          }
+     }
+
+
+})
