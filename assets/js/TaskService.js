f1c1933dd8bd430c151bd4008417c4d0e4c36c11
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-02-21 21:06:52 +0200

    installed Handlebars

--- assets/js/TaskService.js
+++ assets/js/TaskService.js
@@ -1,21 +1,30 @@
-const taskManager =(function(){
-    
-     class Task{
-         constructor(name){
-             this.name=name;
-         }
-     }
-     class TaskManager{
-         constructor(){
-             this.tasks=[];
-         }
-          addTask(another) {
-              
-          }
-          removeTask(another) {
-              
-          }
-     }
+const taskManager = (function () {
 
-
+    class Task {
+        constructor(name, id) {
+            this.name = name;
+            this.id = id;
+        }
+    }
+    class TaskManager {
+        constructor() {
+            this.tasks = [];
+        }
+        addTask(another) {
+            if (another instanceof Task) {
+                this.tasks.push(another);
+            }
+        }
+        removeTask(another) {
+            let index;
+            for (let i = 0; i < this.tasks.length; i++) {
+                if (this.tasks.id === another.id) {
+                    index = i;
+                    break;
+                }
+            }
+            this.tasks.splice(index, 1);
+        }
+    }
+    return new TaskManager();
 })
